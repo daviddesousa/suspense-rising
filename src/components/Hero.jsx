40d65b158c948b15c9725aab7e8174c665713af0
David De Sousa <david.desousa@wewantdance.com>
2025-11-28 10:24:13 -0500

    install motion animation library
    * npm audit fix
    
    * add motion to hero and releases

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,40 +7,54 @@ import suspenseRisingLogo530Png from '../assets/suspense-rising-chrome-unicolor-
 import suspenseRisingLogo795Png from '../assets/suspense-rising-chrome-unicolor-795.png';
 import suspenseRisingLogo1060Png from '../assets/suspense-rising-chrome-unicolor-1060.png';
 
+import { motion as Motion } from 'motion/react';
+
 export default function Hero() {
   return (
     <section className="hero">
       <h1 className="sr-only">Suspense Rising</h1>
-      <picture className="hero-center">
-        <source
-          type="image/webp"
-          srcSet={`
+      <div className="hero-center">
+        <Motion.picture
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+        >
+          <source
+            type="image/webp"
+            srcSet={`
           ${suspenseRisingLogo265Webp},
           ${suspenseRisingLogo530Webp} 2x,
           ${suspenseRisingLogo795Webp} 3x,
           ${suspenseRisingLogo1060Webp} 4x
         `}
-        />
-        <source
-          type="image/png"
-          srcSet={`
+          />
+          <source
+            type="image/png"
+            srcSet={`
           ${suspenseRisingLogo265Png},
           ${suspenseRisingLogo530Png} 2x,
           ${suspenseRisingLogo795Png} 3x,
           ${suspenseRisingLogo1060Png} 4x
         `}
-        />
-        <img
-          src={suspenseRisingLogo265Png}
-          alt="Suspense Rising in chrome lettering"
-          width="265"
-          height="28"
-          className="logo"
-        />
-      </picture>
-      <a className="scroll-down" href="#releases">
+          />
+          <img
+            src={suspenseRisingLogo265Png}
+            alt="Suspense Rising in chrome lettering"
+            width="265"
+            height="28"
+            className="logo"
+          />
+        </Motion.picture>
+      </div>
+      <Motion.a
+        className="scroll-down"
+        href="#releases"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: 'easeOut' }}
+      >
         SCROLL DOWN
-      </a>
+      </Motion.a>
     </section>
   );
 }
