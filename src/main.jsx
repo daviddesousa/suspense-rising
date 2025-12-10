595586e853af9c8f376af408b940c1e0cdd85c66
David De Sousa <david.desousa@wewantdance.com>
2025-12-10 10:15:06 -0500

    remove base from vite and router

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ document.head.appendChild(webpScript);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <Routes>
         <Route element={<App />}>
           <Route index element={null} />
