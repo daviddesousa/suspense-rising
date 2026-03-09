dbb0bc2d55c9ea04b08c47f66d024af288271a19
David De Sousa <david.desousa@wewantdance.com>
2026-03-09 15:13:52 -0400

    working example using buy button sdk

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,6 +2,19 @@ import ShopSection from './ShopSection';
 import Carousel3D from './Carousel3D';
 import HaslowBackground from './HaslowBackground';
 
+import Client from 'shopify-buy';
+
+const client = Client.buildClient({
+  domain: 'suspense-rising.myshopify.com',
+  storefrontAccessToken: 'c233afc68457cdc3b00a15bd9b4bf638',
+});
+
+const handle = 'haslow-tee';
+
+client.product.fetchByHandle(handle).then((product) => {
+  console.log(product);
+});
+
 const SHOP_CONTENT = [
   'He speaks with charm and moves with grace,',
   'but there’s something cold beneath his smile',
