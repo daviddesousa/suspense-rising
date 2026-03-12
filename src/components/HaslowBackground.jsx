593fecc53f38658a523522b51b5e32154aaa2a4a
David De Sousa <david.desousa@wewantdance.com>
2026-03-12 11:47:23 -0400

    add haslow video poster

--- src/components/HaslowBackground.jsx
+++ src/components/HaslowBackground.jsx
@@ -1,5 +1,6 @@
 import haslowMp4 from '../assets/haslow/haslow.mp4';
 import haslowWebm from '../assets/haslow/haslow.webm';
+import haslowPoster from '../assets/haslow/haslow-poster.webp';
 
 const HaslowBackground = () => {
   return (
@@ -11,6 +12,7 @@ const HaslowBackground = () => {
         playsInline
         preload="auto"
         className="w-full h-full object-contain"
+        poster={haslowPoster}
       >
         <source src={haslowMp4} type="video/mp4" />
         <source src={haslowWebm} type="video/webm" />
