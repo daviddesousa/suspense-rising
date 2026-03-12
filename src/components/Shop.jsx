bee419b9baacc573248e9869fcd41e715b34190e
David De Sousa <david.desousa@wewantdance.com>
2026-03-11 20:01:00 -0400

    adjust narrative sentences

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,10 +3,9 @@ import Carousel3D from './Carousel3D';
 import HaslowBackground from './HaslowBackground';
 
 const SHOP_CONTENT = [
-  'He speaks with charm and moves with grace,',
+  'He speaks with charm and moves with grace',
   'but there’s something cold beneath his smile',
-  'like a man who’s witnessed nights he refuses to speak of.',
-  'The closer you get to Haslow, the more it feels like you’re walking into a house without walls,',
+  'The closer you get to Haslow, the more it feels like you’re walking into a house without walls',
   'just layers of smoke and mirrors...',
 ];
 
