0ac8cd5a0165fa53e2f6b988a7c039c0ba37cfe1
David De Sousa <david.desousa@wewantdance.com>
2026-03-17 01:32:16 -0400

    load web analytics production only

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const isProduction = process.env.CONTEXT === 'production'
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          const beaconScript = isProduction
+            ? `
+    <script
+      defer
+      src="https://static.cloudflareinsights.com/beacon.min.js"
+      data-cf-beacon='{"token": "${env.VITE_CLOUDFLARE_TOKEN}"}'
+    ></script>`
+            : ''
+          return html.replace('<!-- CLOUDFLARE_BEACON -->', beaconScript)
+        },
       },
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
+    ],
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
         },
       },
     },
-  },
-  server: {
-    host: '0.0.0.0'
+    server: {
+      host: '0.0.0.0',
+    },
   }
 })
