1a4faffb4c9a953b276976228878e889822e1852
David De Sousa <david.desousa@wewantdance.com>
2026-03-25 09:57:32 -0400

    use vite-imagetools for insta logo

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,5 +1,7 @@
-import brandIconPng from '../assets/ig-icon-ch.png';
-import brandIconWebp from '../assets/ig-icon-ch.webp';
+import brandIconAvif from '../assets/ig-icon-ch.png?w=40;80;120&format=avif&as=srcset';
+import brandIconWebp from '../assets/ig-icon-ch.png?w=40;80;120&format=webp&as=srcset';
+import brandIconJpg from '../assets/ig-icon-ch.png?w=40;80;120&format=jpg&as=srcset';
+import brandIconFallback from '../assets/ig-icon-ch.png?w=40';
 
 export default function SocialLink() {
   return (
@@ -8,17 +10,19 @@ export default function SocialLink() {
       className="brand-link"
       aria-label="Follow Us"
       target="_blank"
-      rel="noreferrer nooepener"
+      rel="noreferrer noopener"
     >
       <picture>
-        <source type="image/webp" srcSet={brandIconWebp} />
-        <source type="image/png" srcSet={brandIconPng} />
+        <source srcSet={brandIconAvif} sizes="40px" type="image/avif" />
+        <source srcSet={brandIconWebp} sizes="40px" type="image/webp" />
+        <source srcSet={brandIconJpg} sizes="40px" type="image/jpeg" />
         <img
-          src={brandIconPng}
+          src={brandIconFallback}
           alt="Follow Us"
           className="brand-icon"
           width="40"
           height="40"
+          decoding="async"
         />
       </picture>
     </a>
