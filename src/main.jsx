bfe61431c60f3995a4ab3c78bd69e9a3c6e45b2f
David De Sousa <david.desousa@wewantdance.com>
2026-03-27 17:49:40 -0400

    lazy load Releases and Shop components

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
+import { StrictMode, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 import App from './App';
 
-import Releases from './components/Releases';
-import Shop from './components/Shop';
+const Releases = lazy(() => import('./components/Releases'));
+const Shop = lazy(() => import('./components/Shop'));
 
 import './index.css';
 
@@ -15,10 +15,24 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<App />}>
           <Route index element={null} />
-          <Route path="releases" element={<Releases />} />
-          <Route path="shop" element={<Shop />} />
+          <Route
+            path="releases"
+            element={
+              <Suspense fallback={null}>
+                <Releases />
+              </Suspense>
+            }
+          />
+          <Route
+            path="shop"
+            element={
+              <Suspense fallback={null}>
+                <Shop />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
